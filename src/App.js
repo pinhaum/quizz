4ac1db078e8cf18cd21f069f4f8475de1c9b593d
Gabriel Ramos <gcrepho@gmail.com>
2021-06-08 20:35:14 -0300

    add first question and initial states

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import React, { useState } from 'react';
+
 function App() {
+  const questions = [
+    {
+      questionText: 'this is a question?',
+      answerOptions: [
+        { answerText: 'no', isCorrect: false },
+        { answerText: 'no', isCorrect: false },
+        { answerText: 'no', isCorrect: false },
+        { answerText: 'yes!', isCorrect: true },
+      ],
+    },
+  ];
+
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+  const [score, setScore] = useState(0);
+
   return (
     <>
-      <h1>asadf movie</h1>
+      <h1>Quizz</h1>
     </>
   );
 }
